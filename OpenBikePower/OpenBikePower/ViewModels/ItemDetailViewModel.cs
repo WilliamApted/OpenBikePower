4ffd55b4f1012e25b0aea87abe5f0a0ee108045e
William Apted <willapted@outlook.com>
2019-10-18 16:26:39 +0100

    First commit.
    Intial commit with creation of project.

---
+++ OpenBikePower/OpenBikePower/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using OpenBikePower.Models;
+
+namespace OpenBikePower.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
