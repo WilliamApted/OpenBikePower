fa8ff1d3f4a75103582ceb88a85cf8c3ea739135
William Apted <william.apted@students.plymouth.ac.uk>
2020-01-22 19:53:07 +0000

    Initial commit for setting up CI pipeline.
    First commit, with the solution setup including Unit testing. This enables the github action pipeline to be setup.

---
+++ OBP/OBP/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OBP
+{
+    public class Class1
+    {
+        public void example() 
+        { 
+        
+        }
+    }
+}
---
+++ OBP/OBPTests/Class1Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OBP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OBP.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void exampleTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
