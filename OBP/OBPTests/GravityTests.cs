d33d1b8ec5062e023c47168f70f509697f7ee19e
William Apted <william.apted@students.plymouth.ac.uk>
2020-01-22 22:11:36 +0000

    Implementation of gravitation force calculations.
    Implementation of first force calculating class.

---
+++ OBP/OBPTests/GravityTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OBP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OBP.Tests
+{
+    [TestClass()]
+    public class GravityTests
+    {
+        [TestMethod()]
+        public void GravitationalForceTest()
+        {
+            Gravity gravityCalculator = new Gravity(10, 60);
+
+            double result = gravityCalculator.GravitationalForce();
+
+            Assert.AreEqual(result, 585, 1);
+        }
+    }
+}
